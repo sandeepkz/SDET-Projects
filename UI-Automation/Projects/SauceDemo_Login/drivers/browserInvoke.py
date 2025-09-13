b28d30f7e6b06caf9807fca52dcd780082402b09
Sandeep-Remote <sandeepkz+remote@users.noreply.github.com>
2025-09-13 17:08:17 +0530

    Removed .idea/ from repo and added to .gitignore

--- UI-Automation/Projects/SauceDemo_Login/drivers/browserInvoke.py
+++ UI-Automation/Projects/SauceDemo_Login/drivers/browserInvoke.py
@@ -2,5 +2,4 @@ from selenium import webdriver
 
 class ChromeInvoke:
     def __init__(self):
-        driver_path = r"C:\Users\sandeep.kumar01\Desktop\chromedriver-win64\chromedriver.exe"
-        driver = webdriver.Chrome(executable_path=driver_path)  # Selenium 3.x style
+        pass
--- UI-Automation/Projects/SauceDemo_Login/tests/test_login.py
+++ UI-Automation/Projects/SauceDemo_Login/tests/test_login.py
@@ -1,3 +1,26 @@
-from SauceDemo_Login.drivers.browserInvoke import ChromeInvoke
+import json
+import time
 
-class Login(ChromeInvoke):
+from selenium import webdriver
+
+class Login:
+    with open('../data/credentials.json') as f:
+        creds = json.load(f)
+
+    username = creds["username"]
+    password = creds["password"]
+
+    driver_path = r"C:\Users\sandeep.kumar01\Desktop\chromedriver-win64\chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=driver_path)  # Selenium 3.x style
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element_by_id('user-name').send_keys(username)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_id('login-button').click()
+    time.sleep(3)
+
+    # 🔹 Verify login success (check URL or element)
+    if "inventory" in driver.current_url:
+        print("✅ Login Successful!")
+    else:
+        print("❌ Login Failed!")
+    driver.close()
